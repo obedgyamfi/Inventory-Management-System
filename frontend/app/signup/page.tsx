fb1ee502b37dff17c04450a8b1900dc5f93c34ea
obedgyamfi <obedgyamfi53@gmail.com>
2024-05-23 20:54:37 +0000

    dir structure changed

---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 max-w-md p-8 bg-white rounded-md shadow-lg">
+        <div className="flex flex-col items-center space-y-3">
+          <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
+          <p className="text-sm text-muted-foreground">
+            Create a new account
+          </p>
+        </div>
+        <div className="grid gap-6">
+          <div className="grid gap-2">
+            <Label htmlFor="firstname">First Name</Label>
+            <Input
+              id="firstname"
+              type="firstname"
+              placeholder="firstname"
+            />
+	 </div>
+	 <div className="grid gap-2">
+	    <Label htmlFor="lastname">Last Name</Label>
+	    <Input
+	      id="lastname"
+	      type="lastname"
+	      placeholder="lastname"
+	   />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="john@example.com"
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" placeholder="********" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            <Input id="confirmPassword" type="password" placeholder="********" />
+          </div>
+        </div>
+        <Button className="w-full">Sign Up</Button>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t border-muted-foreground"></span>
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-white px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <Button variant="outline" className="w-full">
+          Google
+        </Button>
+        <div className="flex justify-center">
+          <span className="text-sm text-muted-foreground">
+            Already have an account?{' '}
+            <Link href="/" className="text-secondary-foreground font-semibold hover:underline">
+              Login
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
